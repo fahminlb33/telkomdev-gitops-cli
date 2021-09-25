c7a3b3a4f5e0eab3b229e3864086bac6ef2a68fc
fahminlb33 <fahminlb33@gmail.com>
2021-09-25 10:52:01 +0700

    Add project files.

---
+++ telkomdev-gitops-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace telkomdev_gitops_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
